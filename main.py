7aa8821b0fd31504960056305f2473aca9750143
DiLucaYVL <165409419+DiLucaYVL@users.noreply.github.com>
2025-09-11 11:46:52 -0300

    alteração na exposição od ip

--- main.py
+++ main.py
@@ -13,13 +13,5 @@ app.register_blueprint(api_bp)
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    os.environ['FLASK_RUN_FROM_CLI'] = 'false'  # força execução única
-    
-    # Verifica se deve usar debug mode
-    debug_mode = os.getenv('FLASK_DEBUG', '0') == '1' or os.getenv('FLASK_ENV') == 'development'
-    
-    print(f"🐛 Debug mode: {debug_mode}")
-    print(f"🌐 Servidor iniciando em http://127.0.0.1:5000")
-    
-    app.run(debug=debug_mode, use_reloader=False, host='127.0.0.1', port=5000)
+if __name__ == '__main__':    
+    app.run(use_reloader=False, host="0.0.0.0", port=8000)
